5aa7bbfd986b4d862b334a0eeaa69f694a0024dc
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2018-07-07 11:22:09 +0300

    Reworking build process.

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM golang:1.10-alpine
+
+MAINTAINER Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
--- build/build.sh
+++ build/build.sh
@@ -22,17 +22,12 @@ if [ -z "${PKG}" ]; then
     echo "PKG must be set"
     exit 1
 fi
-if [ -z "${ARCH}" ]; then
-    echo "ARCH must be set"
-    exit 1
-fi
 if [ -z "${VERSION}" ]; then
     echo "VERSION must be set"
     exit 1
 fi
 
 export CGO_ENABLED=0
-export GOARCH="${ARCH}"
 
 go install                                                         \
     -installsuffix "static"                                        \
